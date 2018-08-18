24e1781f100a5820520c809a446d90d1c28a6c16
abudiman88 <adrian.budiman@gmail.com>
2018-08-18 20:11:36 +1000

    added projects

---
+++ Victorion.Timesheet.Domain/Entities/Timesheet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Victorion.Timesheet.Domain.Entities
+{
+    public class Timesheet
+    {
+        public int TimesheetId { get; set; }
+    }
+}
